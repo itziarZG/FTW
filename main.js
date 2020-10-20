638bb72ce9360d8ff565e76cf33eca2a91f6aa0c
Itziar Zameza García <itziar.zameza@gmail.com>
2020-10-20 18:42:36 +0200

    Add readme and merge fase/1

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-const ten = () => {
-  for (let i = 1; i <= 10; i++) {
-    console.log("patata");
+const writeThis = (word, num) => {
+  for (let i = 1; i <= num; i++) {
+    console.log(word);
   }
 };
-ten();
+
+function writeMyArray(array) {
+  for (let index = 0; index < array.length; index++) {
+    const text = array[index].text;
+    const total = array[index].total;
+
+    writeThis(text, total);
+  }
+}
+
+let myWordList = [];
+// fetch(
+//   "https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json"
+// )
+//   .then((Response) => Response.text())
+fetch(
+  "https://beta.adalab.es/ejercicios-extra/js-funciones-y-parametros-desde-cero/data.json"
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    writeMyArray(data.results);
+  });
